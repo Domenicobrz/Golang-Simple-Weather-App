6213fcc6ca784b3e7e7ab723747a487dc68cb4d3
Domenicobrz <domenicobrz@live.it>
2017-02-18 02:31:16 +0100

    Update main.js, now correctly receives query infos

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 window.addEventListener("load", init);
 
+var queryResult;
+var date, temperature, weather, lat, lon, city, country;
+
 function init() {
+    "use strict";
     xhr = new XMLHttpRequest();
     xhr.addEventListener("load", reqHandler);
     xhr.open("GET", "getweatherinfo");
@@ -8,5 +12,18 @@ function init() {
 }
 
 function reqHandler(evt) {
-    console.log(JSON.parse(evt.responseText));
+    "use strict";
+    console.log(JSON.parse(evt.target.responseText));
+    queryResult = evt.target.responseText;
+
+
+    city = queryResult.results.channel.location.city;
+    country = queryResult.results.channel.location.country;
+    date = queryResult.results.channel.item.condition.date;
+    temperature = queryResult.results.channel.item.condition.temp;
+    weather = queryResult.results.channel.item.condition.text;
+    lat = queryResult.results.channel.item.lat;
+    lon = queryResult.results.channel.item.long;
+
+    var condition_code = queryResult.results.channel.item.condition.code;
 }
