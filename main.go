8f1d214433a9ec6b669f46df675b9da2fd162b21
Domenicobrz <domenicobrz@live.it>
2017-02-23 02:48:32 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -10,11 +11,25 @@ import (
 	"net/url"
 	"strconv"
 
-	"github.com/oschwald/geoip2-golang"
+	geoip2 "github.com/oschwald/geoip2-golang"
 	"github.com/tidwall/gjson"
 )
 
+type IPLocation struct {
+	lat string
+	lon string
+}
+
 func main() {
+	/*
+			example query
+		   select * from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE text="({lat},{lon})")
+
+		       'http://query.yahooapis.com/v1/public/yql?q='
+		                    + encodedQuery + '&format=json
+
+		   http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text%3D%22(45%2C10)%22)&format=json
+	*/
 	serveFile := http.StripPrefix("/res/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/", indexHandler)
 	http.Handle("/res/", serveFile)
@@ -24,26 +39,10 @@ func main() {
 func getWeatherInfo(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content Type", "application/json")
 
-	db, err := geoip2.Open("GeoLite2-City.mmdb")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	// If you are using strings that may be invalid, check that ip is not nil
-	ip := net.ParseIP("80.104.158.156")
-	record, err := db.City(ip)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	/*city := record.City.Names["en"]
-	subdiv := record.Subdivisions[0].Names["en"]
-	country := record.Country.Names["en"]*/
-	lat := strconv.FormatFloat(record.Location.Latitude, 'f', -1, 64)
-	lon := strconv.FormatFloat(record.Location.Longitude, 'f', -1, 64)
+	location := getIPLocation(req)
 
 	yahooquery := "select * from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE text=\"(" +
-		lat + "," + lon + ")\")"
+		location.lat + "," + location.lon + ")\")"
 
 	Url, err := url.Parse("http://query.yahooapis.com/")
 	if err != nil {
@@ -71,8 +70,19 @@ func getWeatherInfo(w http.ResponseWriter, req *http.Request) {
 	  lat = queryResult.results.channel.item.lat;
 	  lon = queryResult.results.channel.item.long;
 	  var condition_code = queryResult.results.channel.item.condition.code;*/
-	result := gjson.GetBytes(body, "query.results.channel.location.city")
-	fmt.Println(result)
+	city := gjson.GetBytes(body, "query.results.channel.location.city")
+	country := gjson.GetBytes(body, "query.results.channel.location.country")
+	date := gjson.GetBytes(body, "query.results.channel.item.condition.date")
+	temperature := gjson.GetBytes(body, "query.results.channel.item.condition.temp")
+	code := gjson.GetBytes(body, "query.results.channel.item.condition.code")
+
+	var dat interface{}
+	if err := json.Unmarshal(body, &dat); err != nil {
+		panic(err)
+	}
+	// fmt.Println(dat)
+
+	fmt.Println(city, country, date, temperature, code)
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
@@ -92,3 +102,30 @@ func indexHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 }
+
+func getIPLocation(req *http.Request) IPLocation {
+	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	// If you are using strings that may be invalid, check that ip is not nil
+	ip := net.ParseIP("80.104.158.156")
+	record, err := db.City(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		we could also gather city location directly from GeoLite's database
+
+		city := record.City.Names["en"]
+		subdiv := record.Subdivisions[0].Names["en"]
+		country := record.Country.Names["en"]
+	*/
+	lat := strconv.FormatFloat(record.Location.Latitude, 'f', -1, 64)
+	lon := strconv.FormatFloat(record.Location.Longitude, 'f', -1, 64)
+
+	return IPLocation{lat, lon}
+}
+
